d5b7bee584c66231191cae8a75e1860ea59402b3
Ryoteer <72623439+Ryoteer@users.noreply.github.com>
2024-05-02 10:46:40 -0300

    Added AI; Added Platforms

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("<color=red>AI</color>")]
+    [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _attackDistance = 1.5f;
+    [SerializeField] private float _chaseDistance = 7.5f;
+    [SerializeField] private Transform[] _patrolNodes;
+    [SerializeField] private float _patrolNodeDist = .5f;
+
+    [Header("<color=red>Values</color>")]
+    [SerializeField] private int _maxHP = 100;
+
+    private int _actualHP;
+
+    private Transform _actualDestination = null;
+
+    private void Awake()
+    {
+        _actualHP = _maxHP;
+
+        if (!_agent)
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+    }
+
+    private void Start()
+    {
+        _actualDestination = GetNode(_actualDestination);
+
+        _agent.SetDestination(_actualDestination.position);
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        _actualHP -= dmg;
+
+        if(_actualHP <= 0)
+        {
+            print($"Oh my God, they killed Kenny!");
+            Destroy(gameObject);
+        }
+        else
+        {
+            print($"<color=red>{name}</color>: Recibí <color=black>{dmg}</color> puntos de daño.");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if((_target.position - transform.position).sqrMagnitude <= Mathf.Pow(_chaseDistance, 2))
+        {
+            _agent.SetDestination(_target.position);
+
+            if(((_target.position - transform.position).sqrMagnitude <= Mathf.Pow(_attackDistance, 2)))
+            {
+                _agent.isStopped = true;
+
+                transform.LookAt(_target.position);
+
+                print($"May thy knife chip and shatter.");
+            }
+            else if (_agent.isStopped)
+            {
+                _agent.isStopped = false;
+            }
+        }
+        else if ((_actualDestination.position - transform.position).sqrMagnitude <= Mathf.Pow(_patrolNodeDist, 2))
+        {
+            _actualDestination = GetNode(_actualDestination);
+
+            _agent.SetDestination(_actualDestination.position);
+        }
+        else
+        {
+            _agent.SetDestination(_actualDestination.position);
+        }
+    }
+
+    private Transform GetNode(Transform actualNode)
+    {
+        Transform newNode = null;
+
+        do
+        {
+            newNode = _patrolNodes[Random.Range(0, _patrolNodes.Length)];
+        }
+        while (newNode == actualNode);
+
+        return newNode;
+    }
+}
---
+++ Assets/Scripts/Scenary/Platform.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Platform : MonoBehaviour
+{
+    [Header("<color=purple>AI</color>")]
+    [SerializeField] private NavMeshSurface _surface;
+    [SerializeField] private NavMeshModifier _modifier;
+
+    [Header("<color=purple>Interaction</color>")]
+    [SerializeField] private float _fadeTime = 3f;
+    [SerializeField] private float _intermission = 2f;
+    [SerializeField] private float _respawnTime = 3f;
+
+    private bool _isActive;
+
+    private Collider _collider;
+    private Material _material;
+
+    private Color _ogColor;
+
+    private void Awake()
+    {
+        if (!_modifier)
+        {
+            _modifier = GetComponent<NavMeshModifier>();
+        }
+
+        _collider = GetComponent<Collider>();
+        _material = GetComponent<Renderer>().material;
+
+        _ogColor = _material.color;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!_isActive)
+        {
+            StartCoroutine(PlatformFade());
+        }
+    }
+
+    private IEnumerator PlatformFade()
+    {
+        _isActive = !_isActive;
+
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / _fadeTime;
+            _material.color = new Color(_ogColor.r, _ogColor.g, _ogColor.b, Mathf.Lerp(1f, 0f, t));
+            yield return null;
+        }
+
+        _material.color = new Color(_ogColor.r, _ogColor.g, _ogColor.b, 0f);
+        _collider.enabled = false;
+
+        //_modifier.enabled = false;
+        //_surface.BuildNavMesh();
+
+        yield return new WaitForSeconds(_intermission);
+
+        t = 0f;
+
+        while(t < 1f)
+        {
+            t += Time.deltaTime / _respawnTime;
+            _material.color = new Color(_ogColor.r, _ogColor.g, _ogColor.b, Mathf.Lerp(0f, 1f, t));
+            yield return null;
+        }
+
+        _material.color = new Color(_ogColor.r, _ogColor.g, _ogColor.b, 1f);
+        _collider.enabled = true;
+
+        //_modifier.enabled = true;
+        //_surface.BuildNavMesh();
+
+        _isActive = !_isActive;
+    }
+}
